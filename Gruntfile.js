509c7d0956d99553e041817a35a8e1499d1584de
Jarmo Seppälä <Jarmo Seppl>
2016-10-17 12:02:55 +0300

    Initial push

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        postcss: {
+            options: {
+                processors: [
+                    require('autoprefixer')({browsers: ['last 3 version']})
+                ]
+            },
+            dist: {
+                src: 'build/style.css'
+            }
+        },
+        sass: {
+            files: ['src/style/0-utility/*', 'src/style/1-base/*', 'src/style/2-blocks/*', 'src/style/3-layout/*'],
+            dist: {
+                options: {
+                    style: 'compressed',
+                    sourcemap: 'none',
+                    noCache: true
+                },
+                files: {
+                    'build/style.css':'src/style/style.scss'
+                }
+            }
+        },
+        concat: {
+            files: ['src/scripts/app.js', 'src/scripts/services/*.js', 'src/scripts/controllers/*.js', 'src/scripts/directives/*.js'],
+            options: {
+                separator: '\n',
+            },
+            dist: {
+                src: ['src/scripts/app.js', 'src/scripts/services/*.js', 'src/scripts/controllers/*.js', 'src/scripts/directives/*.js'],
+                dest: 'build/code.js',
+            },
+        },
+        uglify: {
+            target: {
+                files: {
+                    'build/code.min.js': 'build/code.js'
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            files: ['<%= sass.files %>', '<%= concat.files %>', '**/*.html'],
+            tasks: ['sass', 'concat', 'uglify', 'postcss'],
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 4000,
+                    base: '/Users/Jarmo/development/elearning', // Project folder root, put your own
+                    livereload: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-postcss');
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('server', "Serving the app", [
+        'connect:server', 'watch' ]);
+
+};
